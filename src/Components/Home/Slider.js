7b5910feae7a75e9abec81ba80aed39b1c1c02f2
bakrgit <201740593@fa-hists.edu.eg>
2022-01-02 18:34:44 +0200

    silder image

--- src/Components/Home/Slider.js
+++ src/Components/Home/Slider.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { Carousel } from "react-bootstrap";
 import sliderimg from "../../images/slider1.png";
+import slider4 from "../../images/slider4.png";
 import prod3 from "../../images/prod3.png";
+import prod4 from "../../images/prod4.png";
 const Slider = () => {
   const [index, setIndex] = useState(0);
 
@@ -16,7 +18,7 @@ const Slider = () => {
           <img
             style={{ height: "296px", width: "313.53px" }}
             className=""
-            src={sliderimg}
+            src={slider4}
             alt="First slide"
           />
           <div className="">
@@ -46,6 +48,21 @@ const Slider = () => {
             style={{ height: "296px", width: "373.53px" }}
             className=""
             src={prod3}
+            alt="First slide"
+          />
+          <div className="">
+            <h3 className="slider-title">هناك خصم كبير</h3>
+            <p className="slider-text">خصم يصل ٥٠٪ عند شرائك</p>
+          </div>
+        </div>
+      </Carousel.Item>
+
+      <Carousel.Item className="slider-background4" interval={2000}>
+        <div className="d-flex flex-row justify-content-center align-items-center">
+          <img
+            style={{ height: "296px", width: "373.53px" }}
+            className=""
+            src={prod4}
             alt="First slide"
           />
           <div className="">
